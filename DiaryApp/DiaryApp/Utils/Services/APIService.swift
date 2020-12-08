e5705358422c51a9e3ca007b876c334b7be4c157
Dhara Patel <dhara.patel@drcsystems.com>
2020-12-08 10:42:15 +0530

    [feature] project setup done

---
+++ DiaryApp/DiaryApp/Utils/Services/APIService.swift
@@ -0,0 +1,9 @@
+//
+//  APIService.swift
+//  DiaryApp
+//
+//  Created by Dhara Patel on 08/12/20.
+//  Copyright © 2020 Dhara Patel. All rights reserved.
+//
+
+import Foundation
---
+++ DiaryApp/DiaryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiaryApp
+//
+//  Created by Dhara Patel on 08/12/20.
+//  Copyright © 2020 Dhara Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
